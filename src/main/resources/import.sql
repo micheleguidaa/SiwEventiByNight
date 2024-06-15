bb9c896df096be5ca6f4da2ba0b1b134bea900f1
Alessandro <alessandroschmitt03@gmail.com>
2024-06-15 13:40:18 +0200

    restyling

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@ INSERT INTO Event (id, name, theme, start_date_time, end_date_time, cost, n_max_
 INSERT INTO Event (id, name, theme, start_date_time, end_date_time, cost, n_max_participants, url_image) VALUES (nextval('event_seq'), 'Event2', 'Art', '2024-08-15 09:00:00', '2024-08-15 17:00:00', 30, 50, '/images/event/piper.jpg');
 INSERT INTO Event (id, name, theme, start_date_time, end_date_time, cost, n_max_participants, url_image) VALUES (nextval('event_seq'), 'Event3', 'Music', '2024-09-10 14:00:00', '2024-09-10 22:00:00', 40, 200,'/images/event/spazio900.jpg');
 
-INSERT INTO Local (id, name, address) VALUES (nextval('local_seq'), 'Location1', '123 Main St, Cityville');
-INSERT INTO Local (id, name, address) VALUES (nextval('local_seq'), 'Location2', '456 Elm St, Townsville');
-INSERT INTO Local (id, name, address) VALUES (nextval('local_seq'),'Location3', '789 Oak St, Villageville');
+INSERT INTO Local (id, name, address, urls_images) VALUES (nextval('local_seq'), 'Location1', '123 Main St, Cityville','{/images/local/locale1.jpg}');
+INSERT INTO Local (id, name, address, urls_images) VALUES (nextval('local_seq'), 'Location2', '456 Elm St, Townsville','{/images/local/locale2.jpg}');
+INSERT INTO Local (id, name, address, urls_images) VALUES (nextval('local_seq'),'Location3', '789 Oak St, Villageville','{/images/local/locale3.jpg}');
 
