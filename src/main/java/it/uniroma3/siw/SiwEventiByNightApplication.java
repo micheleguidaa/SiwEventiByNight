8c6aaea6f6910cfe64a956539a5c8bbda0829310
Alessandro <alessandroschmitt03@gmail.com>
2024-06-15 09:32:55 +0200

    primo commit

--- src/main/java/it/uniroma3/siw/SiwEventiByNightApplication.java
+++ src/main/java/it/uniroma3/siw/SiwEventiByNightApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SiwEventiByNightApplication {
 
 	public static void main(String[] args) {
-		//prova
-		//prova3
-		//prova4
-		//prova2
 		SpringApplication.run(SiwEventiByNightApplication.class, args);
 	}
-//prova2
+
 }
