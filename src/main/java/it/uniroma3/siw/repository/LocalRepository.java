dd7d0fa6327a9b4199219d18e94da0824ab593b1
Alessandro <alessandroschmitt03@gmail.com>
2024-06-15 11:27:14 +0200

    aggiunti controller, service, repository per eventi e locali

---
+++ src/main/java/it/uniroma3/siw/repository/LocalRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Local;
+
+public interface LocalRepository extends CrudRepository<Local, Long>{
+	
+}
