7404d123e9c02b79059042b0d61cf1ffa1e3cc73
Alessandro <alessandroschmitt03@gmail.com>
2024-07-01 16:00:42 +0200

    merge

--- src/main/java/it/uniroma3/siw/SiwEventiByNightApplication.java
+++ src/main/java/it/uniroma3/siw/SiwEventiByNightApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SiwEventiByNightApplication {
 
 	public static void main(String[] args) {
-		//prova
+		//prova prova
 		SpringApplication.run(SiwEventiByNightApplication.class, args);
 	}
 
